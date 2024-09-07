7fae9b539def39a724c511a8b21838af9b0948b0
leonard <leonard_liu@foxmail.com>
2024-09-08 00:26:29 +0800

    mybatis用户类测试插入

--- weblog-module-common/src/main/java/com/leonard/weblog/common/config/MybatisPlusConfig.java
+++ weblog-module-common/src/main/java/com/leonard/weblog/common/config/MybatisPlusConfig.java
@@ -3,8 +3,7 @@ package com.leonard.weblog.common.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configura
-tion
+@Configuration
 @MapperScan("com.leonard.weblog.common.domain.mapper")
 public class MybatisPlusConfig {
 }
